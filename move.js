7c0c7612bedfb303a3f5c442a21bcebdc60fda6d
CrowdHailer <peterhsaxton@gmail.com>
2013-12-04 17:38:15 +0000

    Actively get parent size/viewbox dimenstions
    Also reduced to use conditional ternary statements

--- move.js
+++ move.js
@@ -1,35 +1,62 @@
 var svgManoeuvre = {
-	init: function (svgID) {
-		alert('boom');
-		this.figure = document.getElementById(svgID);
-		this.width  = 2000
-        this.height = 1000
-		console.log(this.width);
+	init: function (svgId) {
+		this.transformGroup = document.getElementById(svgId);
+		this.svgElement = document.getElementById('svgDocument');
+		this.view = this.getViewbox(this.svgElement);
+		this.transformGroup.addEventListener('mousedown',function (evt){
+		svgManoeuvre.startMove(evt);
+		},false);
+		this.transformGroup.addEventListener('mousemove',function (evt){
+		svgManoeuvre.moveIt(evt);
+		},false); 
+		this.transformGroup.addEventListener('mouseup',function (evt){
+		svgManoeuvre.endMove(evt);
+		},false); 
 	},
 	transMatrix: [1,0,0,1,0,0],
+	getViewbox: function (svgElement) {
+		return svgElement.getAttribute('viewBox').split(' ');
+	},
 	setMatrix: function (updateMatrix) {
 		if (updateMatrix.length === 6) {
 		strMatrix = "matrix(" +  updateMatrix.join(' ') + ")";
-		this.figure.setAttributeNS(null, "transform", strMatrix);
+		this.transformGroup.setAttributeNS(null, "transform", strMatrix);
 		}
 	},
 	zoom: function (scale) {
-		console.log('zoom');
 		var transMatrix = this.transMatrix;
 		if (scale*transMatrix[0] < 1) { scale = 1 / transMatrix[0]; }
 		for (var i=0; i < transMatrix.length; i++) { transMatrix[i] *= scale; }
-		console.log(this.width);
-		transMatrix[4] += (1-scale)*this.width/2;
-		transMatrix[5] += (1-scale)*this.height/2;
+		transMatrix[4] += (1-scale)*this.view[2]/2;
+		transMatrix[5] += (1-scale)*this.view[3]/2;
 		this.transMatrix = transMatrix;
-		console.log(transMatrix);
 		this.setMatrix(transMatrix);
 	},
 	pan: function (dx, dy) {
 		this.transMatrix[4] += dx;
 		this.transMatrix[5] += dy;
 		this.setMatrix(this.transMatrix);
+	},
+	startMove: function (evt) {
+		this.move = true;
+		this.x1 = evt.clientX;
+		this.y1 = evt.clientY;
+		var xScale = this.view[2]/this.svgElement.offsetWidth;
+		var yScale = this.view[3]/this.svgElement.offsetHeight;
+		this.scale = (yScale > xScale) ? yScale : xScale;
+	},
+	moveIt: function (evt) {
+		if (this.move) {
+			var dx = evt.clientX - this.x1;
+			var dy = evt.clientY - this.y1;
+			this.x1 = evt.clientX;
+			this.y1 = evt.clientY;
+
+			evt.ctrlKey ? this.zoom(Math.pow(2,-dy/100)) : this.pan(this.scale*dx, this.scale*dy);
+		}
+	},
+	endMove: function (evt) {
+		this.move = false
 	}
 };
 svgManoeuvre.init("manoeuvrable-svg");
-svgManoeuvre.zoom(4);
