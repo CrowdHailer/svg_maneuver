dcdaecbc4c481a96f802d6e42e60c0784ff771fd
CrowdHailer <peterhsaxton@gmail.com>
2013-12-06 10:05:02 +0000

    fixed scale problem

--- move.js
+++ move.js
@@ -21,7 +21,6 @@ var svgManoeuvre = {
 		});
 		hammertime.on("touch", function(evt) {
 			svgManoeuvre.startMove(evt);
-			svgManoeuvre.scale = 1;
 		});
 		hammertime.on("drag", function(evt) {
 			evt.gesture.preventDefault()
@@ -34,10 +33,10 @@ var svgManoeuvre = {
 			alert('bosh');
 			svgManoeuvre.endMove(evt); // needed to stop drag which initiated with touch
 		});
-		var hammertime = Hammer(transformGroup).on("transform", function(evt) {
+		/*var hammertime = Hammer(transformGroup).on("transform", function(evt) {
 			svgManoeuvre.scale = evt.gesture.scale/svgManoeuvre.scale //scale relative to touchdown
 			svgManoeuvre.zoom(svgManoeuvre.scale)
-		});
+		});*/
 		function displaywheel(e){ 
 				var evt=window.event || e; //equalize event object 
 				var delta=evt.detail? evt.detail*(-120) : evt.wheelDelta; //check for detail first so Opera uses that instead of wheelDelta 
@@ -81,6 +80,7 @@ var svgManoeuvre = {
 		// Hammer dx and dy properties are related to position at gesture start, therefore must always refer to matrix at start of gesture.
 		this.transMatrix[4] = this.startMatrix[4] + dx;
 		this.transMatrix[5] = this.startMatrix[5] + dy;
+		//console.log(dx);
 		this.setMatrix(this.transMatrix);
 	},
 	startMove: function (evt) {
@@ -88,19 +88,21 @@ var svgManoeuvre = {
 		this.startMatrix = this.transMatrix.slice(0);
 		var xScale = this.view[2]/this.svgElement.offsetWidth;
 		var yScale = this.view[3]/this.svgElement.offsetHeight;
-		console.log(this.svgElement.offsetWidth);
-		console.log(xScale + ' ' + yScale);
-		this.scale = (yScale < xScale) ? yScale : xScale;
+
+		svgManoeuvre.scale = ((yScale > xScale) ? yScale : xScale);
+		console.log(svgManoeuvre.scale);
 	},
 	moveIt: function (evt) {
 		if (this.move) {
 			var dx = evt.gesture.deltaX;
 			var dy = evt.gesture.deltaY;
-			evt.ctrlKey ? this.zoom(Math.pow(2,-dy/100)) : this.pan(this.scale*dx, this.scale*dy);
+			console.log(svgManoeuvre.scale);
+			evt.ctrlKey ? this.zoom(Math.pow(2,-dy/100)) : this.pan(svgManoeuvre.scale*dx, svgManoeuvre.scale*dy);
 		}
 	},
 	endMove: function (evt) {
 		this.move = false
+		console.log(this.transMatrix);
 	}
 };
 svgManoeuvre.init("manoeuvrable-svg");
