dfc5aea2f9585eeac65784d8fac7d1dbbc121df0
CrowdHailer <peterhsaxton@gmail.com>
2013-12-13 09:39:23 +0000

    Drag function implemented

--- move.js
+++ move.js
@@ -1,19 +1,35 @@
 var svgManoeuvre = {
 	init: function (svgElement) {
 		this.svgElement = document.getElementById(svgElement);
-		/*console.log(this.getViewbox());
-		console.log(this.getScale());
-		console.log(this.svgElement.getScreenCTM().inverse());*/
+		var hammertime = Hammer(document).on("drag dragstart dragend", this.eventHandler);
+	},
+	eventHandler: function (evt) {
+		//console.log(evt.type);
+		switch(evt.type) {
+			case ("dragstart"):
+				svgManoeuvre.startDrag(evt);
+			case ("drag"):
+				svgManoeuvre.dragIt(evt);
+		}
+	},
+	startDrag: function (evt) {
+		svgManoeuvre.startViewbox = svgManoeuvre.getViewbox();
+		svgManoeuvre.scale = svgManoeuvre.getScale();
+	},
+	dragIt: function (evt) {
+		var dx = evt.gesture.deltaX;
+		var dy = evt.gesture.deltaY;
+		var scale = svgManoeuvre.scale;
+		svgManoeuvre.pan(scale*dx, scale*dy);
+	},
+	pan: function (dx, dy) {
+		var minX = -dx + +svgManoeuvre.startViewbox[0] ;
+		var minY = -dy + +svgManoeuvre.startViewbox[1];
+		this.svgElement.setAttribute('viewBox', minX + ' ' + minY + ' ' + svgManoeuvre.startViewbox[2] + ' ' + svgManoeuvre.startViewbox[3]);
 	},
 	getViewbox: function () {
 		return this.svgElement.getAttribute('viewBox').split(' ');
 	},
-	/*getScale: function () {
-		var view = this.getViewbox();
-		var xScale = view[2]/this.svgElement.clientWidth;
-		var yScale = view[3]/this.svgElement.clientHeight;
-		return ((yScale > xScale) ? yScale : xScale);
-	}*/
 	getScale: function () {
 		return this.svgElement.getScreenCTM().inverse().a;
 	}
