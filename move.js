953fe02401a1f2fe30531c800253f5d369b946e2
CrowdHailer <peterhsaxton@gmail.com>
2013-12-13 10:04:21 +0000

    Shows event coords Page and Viewbox

--- move.js
+++ move.js
@@ -1,7 +1,7 @@
 var svgManoeuvre = {
 	init: function (svgElement) {
 		this.svgElement = document.getElementById(svgElement);
-		var hammertime = Hammer(document).on("drag dragstart dragend", this.eventHandler);
+		var hammertime = Hammer(document).on("drag dragstart dragend tap", this.eventHandler);
 	},
 	eventHandler: function (evt) {
 		//console.log(evt.type);
@@ -10,6 +10,9 @@ var svgManoeuvre = {
 				svgManoeuvre.startDrag(evt);
 			case ("drag"):
 				svgManoeuvre.dragIt(evt);
+			case ("tap"):
+				console.log(evt.gesture.center.pageX, evt.gesture.center.pageY);
+				console.log(svgManoeuvre.getViewboxCoords(evt.gesture.center));
 		}
 	},
 	startDrag: function (evt) {
@@ -23,12 +26,26 @@ var svgManoeuvre = {
 		svgManoeuvre.pan(scale*dx, scale*dy);
 	},
 	pan: function (dx, dy) {
-		var minX = -dx + +svgManoeuvre.startViewbox[0] ;
-		var minY = -dy + +svgManoeuvre.startViewbox[1];
-		this.svgElement.setAttribute('viewBox', minX + ' ' + minY + ' ' + svgManoeuvre.startViewbox[2] + ' ' + svgManoeuvre.startViewbox[3]);
+		var newView = svgManoeuvre.startViewbox.slice(0);
+		newView[0] -= dx;
+		newView[1] -= dy;
+		svgManoeuvre.setViewbox(newView);
+	},
+	getViewboxCoords: function (center) {
+		var point = svgManoeuvre.svgElement.createSVGPoint();
+		point.x = center.pageX;
+		point.y = center.pageY;
+		return svgManoeuvre.coordinateTransform(point, svgManoeuvre.svgElement);
+	},
+	coordinateTransform: function(screenPoint, SvgObject) {
+		var CTM = SvgObject.getScreenCTM();
+		return screenPoint.matrixTransform( CTM.inverse() );
 	},
 	getViewbox: function () {
 		return this.svgElement.getAttribute('viewBox').split(' ');
+	},
+	setViewbox: function (list) {
+		this.svgElement.setAttribute('viewBox', list.join(' '));
 	},
 	getScale: function () {
 		return this.svgElement.getScreenCTM().inverse().a;
