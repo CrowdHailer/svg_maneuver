80c71f33f26e2fc985b404351b943ba3b63bf8dc
CrowdHailer <peterhsaxton@gmail.com>
2013-12-13 09:09:04 +0000

    Use screen CTM for scale

--- move.js
+++ move.js
@@ -1,18 +1,21 @@
 var svgManoeuvre = {
 	init: function (svgElement) {
 		this.svgElement = document.getElementById(svgElement);
-		console.log(this.getViewbox());
+		/*console.log(this.getViewbox());
 		console.log(this.getScale());
-		console.log(this.svgElement.getScreenCTM().inverse());
+		console.log(this.svgElement.getScreenCTM().inverse());*/
 	},
 	getViewbox: function () {
 		return this.svgElement.getAttribute('viewBox').split(' ');
 	},
-	getScale: function () {
+	/*getScale: function () {
 		var view = this.getViewbox();
 		var xScale = view[2]/this.svgElement.clientWidth;
 		var yScale = view[3]/this.svgElement.clientHeight;
 		return ((yScale > xScale) ? yScale : xScale);
+	}*/
+	getScale: function () {
+		return this.svgElement.getScreenCTM().inverse().a;
 	}
 	
 };
