3b1f68ed1c2ff7a374c7fa9c2549dd1e7cea8706
CrowdHailer <peterhsaxton@gmail.com>
2013-12-13 10:38:55 +0000

    Pinch Transform incorperated

--- move.js
+++ move.js
@@ -1,18 +1,27 @@
 var svgManoeuvre = {
 	init: function (svgElement) {
 		this.svgElement = document.getElementById(svgElement);
-		var hammertime = Hammer(document).on("drag dragstart dragend tap", this.eventHandler);
+		var hammertime = Hammer(document).on("drag dragstart dragend tap transformstart transformend pinch", this.eventHandler);
 	},
+	MAX_VIEWBOX_WIDTH: 2000,
 	eventHandler: function (evt) {
 		//console.log(evt.type);
 		switch(evt.type) {
 			case ("dragstart"):
 				svgManoeuvre.startDrag(evt);
+				break;
 			case ("drag"):
 				svgManoeuvre.dragIt(evt);
+				break;
+			case ("transformstart"):
+				svgManoeuvre.startPinch(evt);
+				break;
+			case ("pinch"):
+				svgManoeuvre.pinchIt(evt);
+				break;
 			case ("tap"):
-				console.log(evt.gesture.center.pageX, evt.gesture.center.pageY);
-				console.log(svgManoeuvre.getViewboxCoords(evt.gesture.center));
+				/*console.log(evt.gesture.center.pageX, evt.gesture.center.pageY);
+				console.log(svgManoeuvre.getViewboxCoords(evt.gesture.center));*/
 		}
 	},
 	startDrag: function (evt) {
@@ -25,21 +34,40 @@ var svgManoeuvre = {
 		var scale = svgManoeuvre.scale;
 		svgManoeuvre.pan(scale*dx, scale*dy);
 	},
+	startPinch: function (evt) {
+		svgManoeuvre.startViewbox = svgManoeuvre.getViewbox();
+	},
+	pinchIt: function (evt) {
+		var gesture = evt.gesture;
+		var zoomAt = svgManoeuvre.getViewboxCoords(gesture.center);
+		svgManoeuvre.zoom(gesture.scale, zoomAt.x, zoomAt.y);
+	},
+	zoom: function (scale, svgX, svgY) {
+		var newView = svgManoeuvre.startViewbox.slice(0);
+		newView[2] /= scale;
+		newView[3] /= scale;
+		newView[0] = (+newView[0] + svgX*(scale-1))/scale;
+		newView[1] = (+newView[1] + svgY*(scale-1))/scale;
+		if (newView[2] <= svgManoeuvre.MAX_VIEWBOX_WIDTH) {
+			svgManoeuvre.setViewbox(newView);
+		}
+		console.log(svgManoeuvre.startViewbox);
+	},
 	pan: function (dx, dy) {
 		var newView = svgManoeuvre.startViewbox.slice(0);
 		newView[0] -= dx;
 		newView[1] -= dy;
 		svgManoeuvre.setViewbox(newView);
 	},
+	coordinateTransform: function(screenPoint, SvgObject) {
+		var CTM = SvgObject.getScreenCTM();
+		return screenPoint.matrixTransform( CTM.inverse() );
+	},
 	getViewboxCoords: function (center) {
 		var point = svgManoeuvre.svgElement.createSVGPoint();
 		point.x = center.pageX;
 		point.y = center.pageY;
 		return svgManoeuvre.coordinateTransform(point, svgManoeuvre.svgElement);
-	},
-	coordinateTransform: function(screenPoint, SvgObject) {
-		var CTM = SvgObject.getScreenCTM();
-		return screenPoint.matrixTransform( CTM.inverse() );
 	},
 	getViewbox: function () {
 		return this.svgElement.getAttribute('viewBox').split(' ');
