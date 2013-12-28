4fe36a937642848c7fbed29c383f159be161bdfe
CrowdHailer <peterhsaxton@gmail.com>
2013-12-28 07:12:15 +0000

    changed from single data check to data in lists

--- plugins/swishload.js
+++ plugins/swishload.js
@@ -2,6 +2,8 @@ svgManoeuvre.plugins.swishLoad = {
 	init: function () {
 		svgManoeuvre.swishLoad = false;
 		
+		this.dataTitle = Object.keys(this.callbackFunctions);
+		
 		//overwrite start handlers for transforms to only activate when there is no call for swish loading
 		svgManoeuvre.gestureHandlers.touch = this.startHandler;
 		svgManoeuvre.gestureHandlers.dragstart = this.startHandler;
@@ -23,15 +25,19 @@ svgManoeuvre.plugins.swishLoad = {
 		svgManoeuvre.swishLoad = false;
 	},
 	holdHandler: function (evt) {
+		var self = svgManoeuvre.plugins.swishLoad;
 		svgManoeuvre.svgMove = false;
 		if (svgManoeuvre.isDescendant(svgManoeuvre.svgElement, evt.target)) {
 			svgManoeuvre.swishLoad = true;
-			console.log(evt.target);
-			var title = (evt.target.getAttribute('data-swipetitle'));
-			if (title) {
-				console.log(title);
-				console.log(evt.target.tagName);
+			var target = evt.target;
+			for (i = 0; i < self.dataTitle.length; i++) {
+				var identifier = target.getAttribute('data-' + self.dataTitle[i]);
+				if (identifier) {
+					self.callbackFunctions[self.dataTitle[i]](identifier);
+					break;
+				}
 			}
+			identifier = identifier || 'DEFAULT';
 		}
 	},
 	dragendHandler: function (evt) {
@@ -39,4 +45,16 @@ svgManoeuvre.plugins.swishLoad = {
 			alert('load up for ' + evt.gesture.direction);
 		}
 	},
+	dataTitle: [],
+	callbackFunctions: {
+		station: function (data) {
+			console.log('smells' + data);
+		},
+		line: function (data) {
+			console.log('boooom' + data);
+		}
+	},
+	buildMenu: function () {},
+	showMenu: function () {},
+	directionCallback: function (data) {}
 };
