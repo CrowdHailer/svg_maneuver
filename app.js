610ec5d1c9719034b06ecf0727769df0d97e363c
CrowdHailer <peterhsaxton@gmail.com>
2013-12-28 07:11:22 +0000

    callbacks dictionary added to app.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 Hammer.plugins.fakeMultitouch();
 //Hammer.plugins.showTouches();
 
-svgManoeuvre.plugins.swishLoad.init();
+var swishlyFunctions = {
+	station: {
+		hold: function (data) {},
+		left: function (data) {},
+		up: function (data) {},
+		down: function (data) {}
+	},
+	line: {
+		hold: function (data) {}
+	}
+
+};
+
+
+svgManoeuvre.plugins.swishLoad.init(swishlyFunctions);
 svgManoeuvre.init("svgDocument", "manoeuvrable-svg");
 svgManoeuvre.plugins.mouseWheel.initMouseWheel();
